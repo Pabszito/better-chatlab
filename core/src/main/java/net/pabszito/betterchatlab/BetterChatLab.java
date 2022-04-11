d98f0f0c0910fef88c1c2bc52e55b01768ce2e26
jonakls <asmot54@hotmail.com>
2022-04-11 09:24:09 -0500

    feat: first commit

---
+++ core/src/main/java/net/pabszito/betterchatlab/BetterChatLab.java
@@ -0,0 +1,21 @@
+package net.pabszito.betterchatlab;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterChatLab extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // On load plugin
+    }
+
+    @Override
+    public void onEnable() {
+        // On enable plugin
+    }
+
+    @Override
+    public void onDisable() {
+        // On disable plugin
+    }
+}
